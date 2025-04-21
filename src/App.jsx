07dcff435c2d5d0f750e3a2a196fc5ead2516ea4
dima.moisieienko <dimonmail09@gmail.com>
2025-04-21 20:25:02 +0300

    working with state

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,86 @@
+import { useState } from "react";
+
 function App() {
+  const [count, setCount] = useState(0);
+  const [isShow, setIsShow] = useState(false);
+
+  function increaseCount() {
+    setCount((prev) => prev + 1);
+  }
+
+  function decreaseCount() {
+    if (count === 0) {
+      return;
+    }
+
+    setCount((prev) => prev - 1);
+  }
+
+  function resetCount() {
+    setCount(0);
+  }
+
   return (
     <>
-      {/* <span className="cross">&times;</span> */}
-      {/* <button>Начать</button> */}
+      {isShow && (
+        <span
+          onClick={() => {
+            setIsShow((prev) => !prev);
+          }}
+          className="cross"
+        >
+          &times;
+        </span>
+      )}
+      {!isShow && (
+        <button
+          onClick={() => {
+            setIsShow((prev) => !prev);
+          }}
+        >
+          Начать
+        </button>
+      )}
 
-      <h1>Vite + React = Love</h1>
+      {isShow && (
+        <>
+          <h1>Vite + React = {count >= 3 && "Love"}</h1>
 
-      {/* <div className="logo-container">
-        <img src="/vite.svg" className="logo" alt="Vite logo" />
-        <p>+</p>
-        <img src="/react.svg" className="logo" alt="React logo" />
-        <p>=</p>
-        <img src="/love.svg" className="logo" alt="Love logo" />
-      </div> */}
+          <div className="logo-container">
+            <img
+              style={count >= 1 ? { filter: "none" } : {}}
+              src="/vite.svg"
+              className="logo"
+              alt="Vite logo"
+            />
+            <p>+</p>
+            <img
+              style={count >= 2 ? { filter: "none" } : {}}
+              src="/react.svg"
+              className="logo"
+              alt="React logo"
+            />
+            <p>=</p>
+            <img
+              style={count >= 3 ? { filter: "none" } : {}}
+              src="/love.svg"
+              className="logo"
+              alt="Love logo"
+            />
+          </div>
 
-      <hr />
+          <hr />
 
-      <div className="card">
-        <p className="count-paragraph">count is 0</p>
-        <div className="increment-buttons">
-          <button>+1</button>
-          <button>-1</button>
-          <button>Reset</button>
-        </div>
-      </div>
+          <div className="card">
+            <p className="count-paragraph">count is {count}</p>
+            <div className="increment-buttons">
+              <button onClick={increaseCount}>+1</button>
+              <button onClick={decreaseCount}>-1</button>
+              <button onClick={resetCount}>Reset</button>
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 }
